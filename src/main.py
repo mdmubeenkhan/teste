9a7c90c850da6ac0da9218630dfb3c816a9533c6
ameen <ameen@gmail.com>
2025-10-28 18:49:29 +0530

    ameeen one

--- src/main.py
+++ src/main.py
@@ -3,6 +3,15 @@
 def main():
   print("main func")
 
-
 def a_one():
-  print("mubeen-a")
+  print("ameen-a")
+
+def two():
+  print("tw0")
+
+def three():
+  print("three")
+
+  """git config --global user.name "ameen"
+git config --global user.email "ameen@gmail.com"
+"""
