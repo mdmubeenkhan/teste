faf43e7f24bc6075347caebb40e6bd29af88680d
Md Mubeen Khan <32162870+mdmubeenkhan@users.noreply.github.com>
2025-10-28 19:19:33 +0530

    Remove unused functions from main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,3 @@
-# main
-
-def main():
-  print("main func")
-
-def main22():
-  print("main 22")
-
-def two():
-  print("two mubeen")
-
-def a_one():
-  print("ameen-a")
-
-# def two():
-#   print("tw0")
-
-def three():
-  print("three")
 
   """git config --global user.name "ameen"
 git config --global user.email "ameen@gmail.com"
